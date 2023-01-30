d2e5bf38a842e3dc8aba7af98e0c106e4f06dd1a
Stefano Duri <duristefano@gmail.com>
2023-01-30 12:39:59 -0300

    Calling Api to bring answers

--- src/components/Indecision.vue
+++ src/components/Indecision.vue
@@ -4,19 +4,45 @@
         <div class="bg-dark"></div>
 
         <div class="indecision-container">
-            <input type="text" placeholder="Make me a question">
+            <input type="text" placeholder="Make me a question" v-model="question">
             <p>Remember to end with a question mark (?) </p>
 
             <div>
-                <h2>Will I be Millionaire?</h2>
-                <h1>Yes, No, ....Maybe</h1>
+                <h2>{{ question }}</h2>
+                <h1>{{ answer }}</h1>
             </div>
         </div>
     </div>
 </template>
 <script>
 export default {
-    name: 'Indecision'
+    name: 'Indecision',
+    data() {
+        return {
+            question: null,
+            answer: null
+        }
+    },
+    methods: {
+        async getAnswer() {
+            this.answer = 'Thinking...'
+            
+            const url = 'https://yesno.wtf/api'
+            const response = await fetch(url)
+            const result =  await response.json()
+            
+            const { answer, image } = result
+
+            this.answer = answer
+        }
+    },
+    watch: {
+        question( value, oldValue ) {
+            if(!value.includes('?')) return
+
+            this.getAnswer()
+        }
+    }
 }
 </script>
 <style scoped>
