1f19c0fabd915052b6dc2499b7cf89792af2f72a
Stefano Duri <duristefano@gmail.com>
2023-01-30 12:04:02 -0300

    Adding first styles and features

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <Indecision />
 </template>
+
+
+<script>
+  import Indecision from './components/Indecision.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      Indecision
+    }
+  }
+</script>
 
 <style scoped>
 
---
+++ src/components/Indecision.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <img src="" alt="bg-image">
+        <div class="bg-dark"></div>
+
+        <div class="indecision-container">
+            <input type="text" placeholder="Make me a question">
+            <p>Remember to end with a question mark (?) </p>
+
+            <div>
+                <h2>Will I be Millionaire?</h2>
+                <h1>Yes, No, ....Maybe</h1>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Indecision'
+}
+</script>
+<style scoped>
+img, .bg-dark {
+    height: 100vh;
+    left: 0px;
+    max-height: 100%;
+    max-width: 100%;
+    position: fixed;
+    top: 0px;
+    width: 100vw;
+}
+
+.bg-dark {
+    background-color: rgba(0, 0, 0, 0.4);
+}
+
+.indecision-container {
+    position: relative;
+    z-index: 99;
+}
+
+input {
+    width: 250px;
+    padding: 10px 15px;
+    border-radius: 5px;
+    border: none;
+}
+input:focus {
+    outline: none;
+}
+
+p {
+    color: white;
+    font-size: 20px;
+    margin-top: 0px;
+}
+
+h1, h2 {
+    color: white;
+}
+
+h2 {
+    margin-top: 150px;
+}
+</style>
